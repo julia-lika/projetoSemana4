b047bf999029473a2256cb9c82a0f5cdb585a664
julia-lika <julia.ishikawa@sou.inteli.edu.br>
2024-03-06 09:19:46 -0300

    modularizando conhecimento (implementando colisões)

--- jogo.js
+++ jogo.js
@@ -15,16 +15,12 @@ class Jogo extends Phaser.Scene {
     // Carrega os assets do jogo
     this.load.tilemapTiledJSON("mapa", "assets/mapa.json");
     this.load.image("grama", "assets/Grass.png");
+    this.load.image("arvore", "assets/Basic_Grass_Biom_things.png");
     this.load.spritesheet("bunny", "assets/Basic Charakter Spritesheet.png", {
       frameWidth: 48,
       frameHeight: 48,
     });
     this.load.image("maca", "assets/apple.png");
-    this.load.image("arvore", "assets/arvore.png");
-    this.load.spritesheet("cow", "assets/Free Cow Sprites.png", {
-      frameWidth: 49,
-      frameHeight: 49,
-    });
   }
 
   create() {
@@ -33,7 +29,14 @@ class Jogo extends Phaser.Scene {
     // Cria o mapa
     this.mapa = this.make.tilemap({ key: "mapa" });
     this.tilesetGrama = this.mapa.addTilesetImage("Grass", "grama");
+    this.tilesetArvore = this.mapa.addTilesetImage("Basic_Grass_Biom_things", "arvore");
+    
+    // Criando layers do mapa
     this.chao = this.mapa.createLayer("chao", this.tilesetGrama, 0, 0);
+    this.arvore = this.mapa.createLayer("arvore", this.tilesetArvore, 0, 0);
+
+    // Ativando a propriedade de colisão feita no TILED
+    this.arvore.setCollisionByProperty({colisor: true});
 
     // Configura o mundo de física do jogo
     this.physics.world.setBounds(
@@ -51,7 +54,12 @@ class Jogo extends Phaser.Scene {
     this.bunny = this.physics.add.sprite(128, 128, "bunny").setScale(0.6);
     this.bunny.body.setCollideWorldBounds(true);
 
+    // Adicionando colisão entre o bunny e a árvore
+    this.physics.add.collider(this.bunny, this.arvore);
+
     this.cursors = this.input.keyboard.createCursorKeys();
+
+  
 
     // Configura as animações
     this.anims.create({
@@ -94,23 +102,6 @@ class Jogo extends Phaser.Scene {
       repeat: -1,
     });
 
-    // Grupo de árvores estáticas
-    this.arvore = this.physics.add.staticGroup();
-
-    this.listaArvores = [
-      this.arvore.create(50, 50, "arvore"),
-      this.arvore.create(128, 50, "arvore"),
-      this.arvore.create(206, 50, "arvore"),
-      this.arvore.create(50, 130, "arvore"),
-      this.arvore.create(206, 130, "arvore"),
-      this.arvore.create(50, 210, "arvore"),
-      this.arvore.create(128, 210, "arvore"),
-      this.arvore.create(206, 210, "arvore"),
-    ];
-
-    // Colisão com árvores
-    this.physics.add.collider(this.bunny, this.arvore);
-
     // Configuração da maçã
     this.maca = this.physics.add.sprite(128, 128, "maca");
     this.maca.setCollideWorldBounds(true);
@@ -122,21 +113,23 @@ class Jogo extends Phaser.Scene {
         fontSize: "15px",
         fill: "#495613",
       })
-      .setOrigin(0, 0);
+      .setOrigin(0.5, 2.5);
 
     // Detecta colisão entre o bunny e a maca
     this.physics.add.overlap(this.bunny, this.maca, () => {
       this.maca.setVisible(false);
-
+  
       // Define nova posição para a maçã
-      this.posicaoMaca_Y = Phaser.Math.RND.between(40, 230);
-      this.maca.setPosition(this.posicaoMaca_Y, 100);
-
+      this.posicaoMaca_X = Phaser.Math.RND.between(40, 230);
+      this.posicaoMaca_Y = Phaser.Math.RND.between(40, 400); // Adicionando eixo Y
+  
+      this.maca.setPosition(this.posicaoMaca_X, this.posicaoMaca_Y);
+  
       this.pontuacao += 1;
       this.placar.setText("Maçãs:" + this.pontuacao);
       
       this.maca.setVisible(true);
-    });
+  });
   }
 
   update() {
